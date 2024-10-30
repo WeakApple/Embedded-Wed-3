226202a67a722b0ef5ed4f92deb782a151622182
JinWu <82016681+WeakApple@users.noreply.github.com>
2024-10-30 11:27:58 +0900

    Add: exercise folder
    add exercise folder and files

---
+++ Wed/module_exam/module_exam/module_exam.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int module_exam_init(void) {
+    printk(KERN_INFO "module_init!\n");
+    return 0;
+}
+
+static void module_exam_exit(void) {
+    
+    printk(KERN_INFO "module_exit\n");
+}
+
+module_init(module_exam_init);
+module_exit(module_exam_exit);
+MODULE_LICENSE("GPL");
