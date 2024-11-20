03f2efd40620afdbe6fe898d0913a43e4c9908e7
JinWu <gin01226@naver.com>
2024-11-20 11:39:39 +0900

    update: timer.c

--- Wed/termProject/timer.c
+++ Wed/termProject/timer.c
@@ -11,13 +11,15 @@
 
 int sw[4] = {4, 17, 27, 22};
 int led[4] = {23, 24, 25, 1}, flag = 0;
+int mode = 0; // 0: 전체 off 1: 전체모드, 2: 개별모드,
+static spinlock_t lock;
 
 static struct timer_list timer;
 
 static void timer_cb(struct timer_list * timer) {
     int ret, i;
     printk(KERN_INFO "timer callback function\n");
-
+    spin_lock(&lock);
     if (flag == 0) {
         for(i = 0; i < 4; i++) {
             ret = gpio_direction_output(led[i], HIGH);
@@ -32,18 +34,28 @@ static void timer_cb(struct timer_list * timer) {
 
     timer->expires = jiffies + HZ * 2;
     add_timer(timer);
-
+    spin_unlock(&lock);
 }
 
 
 irqreturn_t irq_handler(int irq, void*dev_id) {
     printk(KERN_INFO"Debug%d\n", irq);
-
     switch(irq) {
         case 60:
+            if(mode == 0){
+                break;
+            }
+            else{
+                del_timer_sync(&timer)
+                for (int i = 0; i < 4; i++) {
+                    gpio_set_value(led[i], LOW); // LED 끄기
+                }
+            }
+
             timer_setup(&timer, timer_cb, 0);
             timer.expires = jiffies + HZ * 2;
             add_timer(&timer);
+            mode = 1
             break;
         case 61:
             printk(KERN_INFO"sw2 interrupt ocurred!\n");
@@ -53,6 +65,11 @@ irqreturn_t irq_handler(int irq, void*dev_id) {
             break;
         case 63:
             printk(KERN_INFO"sw4 interrupt ocurred!\n");
+            del_timer_sync(&timer)
+            for (int i = 0; i < 4; i++) {
+                gpio_set_value(led[i], LOW); // LED 끄기
+            }
+            mode = 0
             break;
     }
     return 0;
@@ -65,7 +82,7 @@ static int led_switch_init(void) {
     for(i = 0; i < 4; i++) {
         res = gpio_request(sw[i], "sw");
         ret = gpio_request(led[i], "LED");
-
+        
         res = request_irq(gpio_to_irq(sw[i]), (irq_handler_t)irq_handler,IRQF_TRIGGER_RISING, "IRQ", (void *)(irq_handler));
         
         if (res < 0)
@@ -82,7 +99,7 @@ static void led_switch_exit(void) {
 
     printk(KERN_INFO "led_switch_exit\n");
 
-    del_timer(&timer);
+    del_timer_sync(&timer);
 
     for(i = 0; i < 4; i++) {
         gpio_free(led[i]);
