c09f839919009fc3c31c345bd0f739766dbce2e5
lee0806 <mh080411@kyonggi.ac.kr>
2024-11-20 11:23:12 +0900

    timer.c

--- Wed/termProject/timer.c
+++ Wed/termProject/timer.c
@@ -15,17 +15,17 @@ int led[4] = {23, 24, 25, 1}, flag = 0;
 static struct timer_list timer;
 
 static void timer_cb(struct timer_list * timer) {
-    int ret, i;
+    int ret_led, i;
     printk(KERN_INFO "timer callback function\n");
 
     if (flag == 0) {
         for(i = 0; i < 4; i++) {
-            ret = gpio_direction_output(led[i], HIGH);
+            ret_led = gpio_direction_output(led[i], HIGH);
         }
         flag = 1;
     } else {
         for(i = 0; i < 4; i++) {
-            ret = gpio_direction_output(led[i], LOW);
+            ret_led = gpio_direction_output(led[i], LOW);
         }
         flag = 0;
     }
@@ -36,41 +36,43 @@ static void timer_cb(struct timer_list * timer) {
 }
 
 
+
+
 irqreturn_t irq_handler(int irq, void*dev_id) {
     printk(KERN_INFO"Debug%d\n", irq);
 
     switch(irq) {
-        case 60:
+        case 61:
             timer_setup(&timer, timer_cb, 0);
             timer.expires = jiffies + HZ * 2;
             add_timer(&timer);
             break;
-        case 61:
+        case 62:
             printk(KERN_INFO"sw2 interrupt ocurred!\n");
             break;
-        case 62:
+        case 63:
             printk(KERN_INFO"sw3 interrupt ocurred!\n");
             break;
-        case 63:
-            printk(KERN_INFO"sw4 interrupt ocurred!\n");
+        case 64:
+            printk(KERN_INFO"sw4 dinterrupt ocurred!\n");
             break;
     }
     return 0;
 }
 
 static int led_switch_init(void) {
-    int res, ret, i;
+    int res_sw, ret_led, i;
     printk(KERN_INFO"led_switch_init!\n");
 
     for(i = 0; i < 4; i++) {
-        res = gpio_request(sw[i], "sw");
-        ret = gpio_request(led[i], "LED");
+        res_sw = gpio_request(sw[i], "sw");
+        ret_led = gpio_request(led[i], "LED");
 
-        res = request_irq(gpio_to_irq(sw[i]), (irq_handler_t)irq_handler,IRQF_TRIGGER_RISING, "IRQ", (void *)(irq_handler));
+        res_sw = request_irq(gpio_to_irq(sw[i]), (irq_handler_t)irq_handler,IRQF_TRIGGER_RISING, "IRQ", (void *)(irq_handler));
         
-        if (res < 0)
+        if (res_sw < 0)
             printk(KERN_INFO"requset_irq failed!\n");
-        if (ret < 0)
+        if (ret_led < 0)
             printk(KERN_INFO "led_module gpio_request failed!\n");
     
     }
