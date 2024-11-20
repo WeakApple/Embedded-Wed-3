3ae75852282ab1d4e7757ecff9078d299400aca4
lee0806 <mh080411@kyonggi.ac.kr>
2024-11-20 11:00:55 +0900

    timer.c

--- Wed/termProject/timer.c
+++ Wed/termProject/timer.c
@@ -1,93 +1,55 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/intterupt.h>
+#include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
-#include <linux/timer.h>
 
-#define HIGH 1
-#define LOW 0
-
-int led[4] = {23, 24, 25, 1}, flag = 0;
 int sw[4] = {4, 17, 27, 22};
-static int mod = 0;
 
-static struct timer_list timeer;
+irqreturn_t irq_handler(int irq, void*dev_id) {
+    printk(KERN_INFO"Debug%d\n", irq);
 
-irqreturn_t irq_handler(int irq, void *dev_id){
-    switch (irq)
-    {
-        case 59:
-            timer_setup(&timer, timer_cb, 0);
-            timer.expires = jiffies + HZ * 2;
-            add_timer(&timer);
-        break;
+    switch(irq) {
         case 60:
-        break;
+            printk(KERN_INFO"sw1 interrupt ocurred!\n");
+            break;
         case 61:
-        break;
+            printk(KERN_INFO"sw2 interrupt ocurred!\n");
+            break;
         case 62:
-        break;
+            printk(KERN_INFO"sw3 interrupt ocurred!\n");
+            break;
+        case 63:
+            printk(KERN_INFO"sw4 interrupt ocurred!\n");
+            break;
     }
     return 0;
 }
 
+static int switch_interrupt_init(void) {
+    int res, i;
+    printk(KERN_INFO"switch_interrupt_init!\n");
 
-static void timer_cb(struct timer_list *timer) {
-    int ret, i;
-    printk(KERN_INFO "timer callback function!\n");
-
-    if(flag == 0) {
-        for(i = 0; i < 4; i++) {
-            ret = gpio_direction_output(led[i], HIGH);
-        }
-        flag = 1;
-    } else {
-        for(i = 0; i < 4; i++) {
-            ret = gpio_direction_output(led[i], LOW);
-        }
-        flag = 0;
-    }
-
-    timer->expires = jiffies + HZ * 2;
-    add_timer(timer);
-}
-
-
-static int switch_interrupt_init(void){
-    int rsw, rled,i;
-    printk(KERN_INFO "sw it init\n");
-    for(i=0;i<4;i++){
-        rsw = gpio_request(sw[i], "SW");
-        rsw = request_irq(gpio_to_irq(sw[i]), (irq_handler_t)irq_handler,IRQF_TRIGGER_RISING, "IRQ", (void *)(irq_handler));
-        if(rsw<0){
-            printk(KERN_INFO "request_irq failed!\n");
-        }
-    }
-    for(i=0;i<4;i++){
-        rled = gpio_request(led[i], "LED");
-        if(ret<0){
-            printk(KERN_INFO "led module rq failed\n");
-        }
+    for(i = 0; i < 4; i++) {
+        res = gpio_request(sw[i], "sw");
+        res = request_irq(gpio_to_irq(sw[i]), (irq_handler_t)irq_handler,IRQF_TRIGGER_RISING, "IRQ", (void *)(irq_handler));
+        if(res<0)
+            printk(KERN_INFO"requset_irq failed!\n");
+    
     }
     return 0;
 }
 
-static void switch_interrupt_exit(void){
+static void switch_interrupt_exit(void) {
     int i;
-    printk(KERN_INFO "sw it exti\n");
-    for(i=0;i<4;i++){
+    printk(KERN_INFO"switch_interrupt_exit!\n");
+    for(i = 0; i < 4; i++) {
         free_irq(gpio_to_irq(sw[i]), (void *)(irq_handler));
         gpio_free(sw[i]);
     }
-    for(i=0;i<4;i++){
-        gpio_free(led[i]);
-    }
 }
-
-
 
 module_init(switch_interrupt_init);
 module_exit(switch_interrupt_exit);
-MODULE_LICENSE("GPL")
+MODULE_LICENSE("GPL");
